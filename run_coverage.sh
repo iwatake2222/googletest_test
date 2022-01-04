2a14b0838864429e992fa4021e5a0ab454aa9496
iwatake2222 <take.iwiw2222@gmail.com>
2022-01-05 03:11:24 +0900

    Add coverage to GitHub Actions

--- run_coverage.sh
+++ run_coverage.sh
@@ -11,6 +11,6 @@ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=--coverage -DCMAKE_CXX_FLAGS=
 make -j4
 ctest
 
-rm -rf coverage; mkdir coverage
-gcovr -r `pwd`/\.\./ -e `pwd` -e ".*/test/.*" -e ".*/third_party/.*"
-gcovr -r `pwd`/\.\./ -e `pwd` -e ".*/test/.*" -e ".*/third_party/.*" --html-details coverage/index.html
+rm -rf coverage && mkdir coverage
+gcovr -r ../ -e `pwd` -e ".*/test/.*" -e ".*/third_party/.*"
+gcovr -r ../ -e `pwd` -e ".*/test/.*" -e ".*/third_party/.*" --html --html-details -o 'coverage/index.html'
